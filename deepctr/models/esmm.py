44ea09a8c5de7eb01e6e5163b83ee040c2b58540
Xierry <41768470+Xierry@users.noreply.github.com>
2020-05-16 16:21:15 +0800

    增加主模型的输出

--- deepctr/models/esmm.py
+++ deepctr/models/esmm.py
@@ -6,14 +6,16 @@ from ..layers.utils import concat_func, reduce_sum
 from ..layers.core import DNN, PredictionLayer
 
 
-def ESMM(user_feature_columns, item_feature_columns, other_feature_columns=None, mask_feat_list=(), 
+def ESMM(user_feature_columns, item_feature_columns, context_feature_columns=None, mask_feat_list=(), 
          dnn_hidden_units = (200, 80), dnn_activation="relu", dnn_dropout=0.0, dnn_use_bn=False, 
          l2_reg_embedding=1e-6, dnn_l2_reg=0.0, init_std=0.0001, task='binary', seed=1024):
 
     # 嵌入字典
+    feature_columns = user_feature_columns + item_feature_columns
+    if context_feature_columns is not None:
+        feature_columns += context_feature_columns
     embedding_dict = create_embedding_matrix(
-        user_feature_columns + item_feature_columns,
-        l2_reg_embedding, init_std, seed, seq_mask_zero=True)
+        feature_columns, l2_reg_embedding, init_std, seed, seq_mask_zero=True)
 
     user_input_dict = build_input_features(user_feature_columns) # -> (None, 1), (None, E)
     item_input_dict = build_input_features(item_feature_columns) # -> (None, 1)
@@ -21,11 +23,19 @@ def ESMM(user_feature_columns, item_feature_columns, other_feature_columns=None,
     user_inputs_list = list(user_input_dict.values())
     item_inputs_list = list(item_input_dict.values())
 
-    user_field_pooled, user_dense_value_list = get_emb_list(user_feature_columns, embedding_dict, user_input_dict, mask_feat_list)
-    item_field_pooled, item_dense_value_list = get_emb_list(item_feature_columns, embedding_dict, item_input_dict, mask_feat_list)
+    user_field_sum, user_dense_value_list = get_emb_list(user_feature_columns, embedding_dict, user_input_dict, mask_feat_list)
+    item_field_sum, item_dense_value_list = get_emb_list(item_feature_columns, embedding_dict, item_input_dict, mask_feat_list)
 
-    dnn_input = combined_dnn_input([user_field_pooled, item_field_pooled], 
-        user_dense_value_list + item_dense_value_list) # -> (None, E_dnn)
+    embedding_list = [user_field_sum, item_field_sum]
+    dense_value_list = user_dense_value_list + item_dense_value_list
+    if context_feature_columns is not None:
+        context_input_dict = build_input_features(context_feature_columns) # -> (None, 1)
+        context_inputs_list = list(context_input_dict.values())
+        context_field_sum, context_dense_value_list = get_emb_list(context_feature_columns, embedding_dict, context_input_dict, mask_feat_list)
+        embedding_list.append(context_field_sum)
+        dense_value_list += context_dense_value_list
+
+    dnn_input = combined_dnn_input(embedding_list, dense_value_list) # -> (None, E_dnn)
 
     # pCVR part
     dnn_output = DNN(dnn_hidden_units, dnn_activation, dnn_l2_reg, dnn_dropout,
@@ -42,9 +52,17 @@ def ESMM(user_feature_columns, item_feature_columns, other_feature_columns=None,
     # pCTCVR part
     pctcvr = tf.multiply(pctr, pcvr)
 
-    model = tf.keras.models.Model(inputs=user_inputs_list+item_inputs_list, outputs=[pctr, pctcvr])
+    inputs = user_inputs_list+item_inputs_list
+    if context_feature_columns is not None:
+        inputs += context_inputs_list
+    model = tf.keras.models.Model(
+        inputs=inputs, outputs=[pctr, pctcvr])
 
-    return model
+    pcvr_model = tf.keras.models.Model(
+        inputs=inputs, outputs=[pcvr])
+
+    # model.get_layer("input_name").get_weights()
+    return model, pcvr_model
 
 
 def get_emb_list(feature_columns, embedding_dict, input_dict, mask_feat_list):
@@ -58,48 +76,11 @@ def get_emb_list(feature_columns, embedding_dict, input_dict, mask_feat_list):
 
     varlen_sparse_emb_dict = varlen_embedding_lookup(
         embedding_dict, input_dict, varlen_sparse_feature_columns)
-    varlen_sparse_emb_list = get_varlen_pooling_list(
-        varlen_sparse_emb_dict, input_dict, varlen_sparse_feature_columns, to_list=True)
-                               
+    varlen_sparse_emb_list = list(get_varlen_pooling_list(
+        varlen_sparse_emb_dict, input_dict, varlen_sparse_feature_columns, to_list=True))
+                      
     # [None, num_feat, E]  <- (None, 1, E)       (None, 1, E)
     emb_concated = concat_func(sparse_emb_list + varlen_sparse_emb_list, axis=-2)
-    field_pooled = reduce_sum(emb_concated, axis=-2) # -> [None, E]
+    field_sum = reduce_sum(emb_concated, axis=-2) # -> [None, E]
 
-    return field_pooled, dense_value_list
-
-
-if __name__ == "__main__":
-    import numpy as np
-    user_feature_columns = [
-        DenseFeat('pay_score', 1),
-        SparseFeat('user', 4, embedding_dim=4),
-        SparseFeat('gender', 2, embedding_dim=4),
-        VarLenSparseFeat(
-            SparseFeat('hist_item_id', 3 + 1, embedding_dim=4,embedding_name='item_id'),
-            maxlen=4, 
-            # length_name="seq_length"
-        ),
-        VarLenSparseFeat(
-            SparseFeat('hist_cate_id', 2 + 1, embedding_dim=4,embedding_name='cate_id'),
-            maxlen=4, 
-            # length_name="seq_length"
-        )]
-
-    item_feature_columns = [
-        SparseFeat('item_id', 3 + 1, embedding_dim=4),
-        SparseFeat('cate_id', 2 + 1, embedding_dim=4),
-    ]
-
-    X = {
-        'user':     np.array([0, 1, 2, 3]), 
-        'gender':   np.array([0, 1, 0, 1]), 
-        'item_id':  np.array([1, 2, 3, 2]) , 
-        'cate_id':  np.array([1, 2, 1, 2]),
-        'hist_item_id': np.array([[1, 2, 3, 0], [1, 3, 2, 1], [3, 2, 0, 0], [2, 1, 0, 0]]), 
-        'hist_cate_id': np.array([[1, 1, 2, 0], [2, 1, 1, 2], [1, 2, 0, 0], [2, 1, 0, 0]]),
-        'pay_score': np.array([0.1, 0.2, 0.3, 0.2]), 
-        'seq_length': np.array([3, 4, 2, 2])
-    }
-
-    y_ctr = np.array([1, 1, 1, 0])
-    y_ctcvr = np.array([1, 0, 1, 0])
+    return field_sum, dense_value_list
